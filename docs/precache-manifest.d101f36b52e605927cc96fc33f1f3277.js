9783ef1a2cd04c8d03c7b9b41ef9f5907abc5a02
anyenaia <jabed2806@gmail.com>
2020-08-21 06:52:06 -0500

    docs commit

---
+++ docs/precache-manifest.d101f36b52e605927cc96fc33f1f3277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e01ac1014cf6580555c77e0953e72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b22d45de5b85eb53682c",
+    "url": "/static/css/main.850d848f.chunk.css"
+  },
+  {
+    "revision": "a8e134c2bfd5ad868631",
+    "url": "/static/js/2.fd2f0d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd2f0d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22d45de5b85eb53682c",
+    "url": "/static/js/main.bc623786.chunk.js"
+  },
+  {
+    "revision": "42ca63168b8c659a47df",
+    "url": "/static/js/runtime-main.878f8912.js"
+  }
+]);
